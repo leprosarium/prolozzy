4a732275d65850a55e63421547e207b48b7a0c55
Anton <leprosarium@gmail.com>
2013-04-04 21:58:50 +0400

    Minor removing of defines

--- Game/DizDebug.h
+++ Game/DizDebug.h
@@ -16,8 +16,8 @@
 const size_t CON_LINES = 2048;	// total lines
 const size_t SLOT_COUNT = 16;	// total slots
 const size_t INFO_LINES = 2;	// info slot lines
-#define INPUT_SIZE			256		// input cmd size
-#define INPUT_HISTORY		16		// input history count
+const size_t INPUT_SIZE = 256;	// input cmd size
+const size_t INPUT_HISTORY = 16;// input history count
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 // cDizDebug
